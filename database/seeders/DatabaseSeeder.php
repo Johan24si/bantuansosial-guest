2f29e88537c1fd813b69999089aa1b23349098c9
Johan24si <johan24si@mahasiswa.pcr.ac.id>
2025-11-23 21:32:22 +0700

    ubah sedikit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,21 +6,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // Contoh factory bawaan
-        // User::factory(10)->create();
-
-        // User default contoh
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // 👉 Panggil seeder yang kamu buat di sini
         $this->call([
             UserSeeder::class,
         ]);
