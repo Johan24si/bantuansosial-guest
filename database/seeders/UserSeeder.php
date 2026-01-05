bc23cc55128f689db6d4e63772dfd28da5fa9745
givopcr <givo24si@mahasiswa.pcr.ac.id>
2026-01-05 11:00:33 +0700

    tes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,16 +10,13 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Buat 1 user khusus
-        User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@gmail.com',
+        User::create([
+            'name'  => 'fmi',
+            'email' => 'fni',
             'role'  => 'admin', // wajib disesuaikan dengan model
         ]);
 
-        // Buat banyak user random
-        User::factory(100)->create([
-            'role' => 'user', // default role untuk mass seeding
-        ]);
+
     }
 }
 
